aae1ae4e0325cf923a57e9f7f8eaa03ac72157ea
sarahshi <sshi16@spenceschool.org>
2016-04-02 17:39:46 +0000

    Done.

--- js/ticket.js
+++ js/ticket.js
@@ -1,15 +1,17 @@
 //Code Your Solution Here
 
-var ticket_template = "Joe Shmoe flying on Fancy Airlines Flight Number 000 arriving at Fancy Destination at 2400 hours. The flight will depart from gate 100.
+var ticket_template = "Joe Shmoe flying on Fancy Airlines Flight Number 000 arriving at Fancy Destination at 2400 hours. The flight will depart from gate 100.";
 
+// ticket_template = ticket_template.replace("Joe Shmoe", "Bessie Baker").replace("Fancy Airlines", "Delta").replace("000", "939").replace("Fancy Desitination", "Atlanta").replace("2400 hours", "8pm").replace("100", "B10");
 
-var real_ticket = ticket_template.replace("Joe Shmoe", "Bessie Baker");
-var real_ticket1 = ticket_template.replace("Fancy Airlines", "Delta");
-var real_ticket2 = ticket_template.replace("000", "939");
-var real_ticket3 = ticket_template.replace("Fancy Destination", "Atlanta");
-var real_ticket4 = ticket_template.replace("Joe Shmoe", "Bessie Baker");
-var real_ticket5 = ticket_template.replace("Joe Shmoe", "Bessie Baker");
-var real_ticket6 = ticket_template.replace("2400", "8pm");
-var real_ticket7 = ticket_template.replace("100", "B10");
+var name = "Mother";
+var destination = "San Francisco";
+var airline = "United Airlines";
+var flight_arrival = "1015am";
+var gate = "A62";
 
-alert(real_ticket);
+ticket_template = ticket_template.replace("Joe Shmoe", name.toUpperCase()).replace("Fancy Airlines", airline).replace("Fancy Destination", destination).replace("2400 hours", flight_arrival).replace("100", gate);
+
+ticket_template = ticket_template + " on Fancy Departure Date"
+
+alert(ticket_template)
